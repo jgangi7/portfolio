cf96b4788ab988120c7516a0ed29b6442fdb6bfc
jgangi7 <gangijames18@gmail.com>
2025-03-04 13:03:19 -0500

    v1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['three'],
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }];
+    return config;
+  },
+  i18n: {
+    locales: ['en', 'fr', 'de'],
+    defaultLocale: 'en',
+    localeDetection: true,
+  },
 }
 
 module.exports = nextConfig; 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
 import { ThemeProvider } from 'next-themes'
+import { NextIntlClientProvider } from 'next-intl'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
+
+// Import all messages
+import en from '../messages/en.json'
+import fr from '../messages/fr.json'
+import de from '../messages/de.json'
+
+const messages = { en, fr, de }
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = useState(false)
+  const router = useRouter()
+  const locale = router.locale || 'en'
 
   // Ensure theme switching works properly with SSR
   useEffect(() => {
@@ -16,9 +27,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <NextIntlClientProvider messages={messages[locale as keyof typeof messages]} locale={locale}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </NextIntlClientProvider>
   )
 }
 
