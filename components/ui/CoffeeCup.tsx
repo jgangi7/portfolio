443419915284724c8fe1e0eb804f54330a645f35
jgangi7 <gangijames18@gmail.com>
2025-04-07 11:50:12 -0400

    integrated buymeacoffee!

---
+++ components/ui/CoffeeCup.tsx
@@ -0,0 +1,16 @@
+export const CoffeeCup = () => {
+  return (
+    <a
+      href="https://buymeacoffee.com/gangijamesn"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="inline-block"
+    >
+      <img
+        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+        alt="Buy Me A Coffee"
+        className="h-8 w-auto"
+      />
+    </a>
+  );
+}; 
