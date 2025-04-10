174ac39791c7d4957d73969013e4ce9feeeef1a3
jgangi7 <gangijames18@gmail.com>
2025-04-10 15:28:54 -0400

    added spline scene

---
+++ components/ui/SplineScene.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export const SplineScene = () => {
+  return (
+    <div className="hidden sm:block w-full h-full">
+      <Spline scene="https://prod.spline.design/LzUyA5zq-85OTHPe/scene.splinecode" />
+    </div>
+  );
+};
