4d21043470e9db1ecfd10ed6ff6986e23b8cdab4
jgangi7 <gangijames18@gmail.com>
2025-04-12 12:15:29 -0400

    added hero text back

--- components/ui/SplineScene.tsx
+++ components/ui/SplineScene.tsx
@@ -2,8 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export const SplineScene = () => {
   return (
-    <div className="w-full h-full max-w-screen-xl mx-auto px-4">
       <Spline scene="https://prod.spline.design/LzUyA5zq-85OTHPe/scene.splinecode" />
-    </div>
   );
 };
